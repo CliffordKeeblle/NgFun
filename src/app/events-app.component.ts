472ec10117b76cbd40deeb27868554a146988f4d
CliffordKeeble <cliffordkeeble@hotmail.com>
2019-08-07 09:01:26 +0100

    Add logo

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
   template: `
-    Hello Cliff
+    <h1><img src="assets/images/dev.png" alt="logo" height ="100em" />  Code Therapy</h1>
     <events-list></events-list>
+
   `
 })
 export class EventsAppComponent {
