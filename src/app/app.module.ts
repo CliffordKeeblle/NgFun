1ddcaf74e96f36d42953808a4bbcefd2e706949e
CliffordKeeble <cliffordkeeble@hotmail.com>
2019-08-07 11:14:34 +0100

    Section 04  Nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent} from "./nav/nav-bar.component"
 
 @NgModule({
   declarations: [
-    EventsAppComponent, EventsListComponent, EventThumbnailComponent
+    EventsAppComponent, EventsListComponent, EventThumbnailComponent, NavBarComponent
   ],
   imports: [
     BrowserModule
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
   template: `
+    <nav-bar></nav-bar>
     <h1><img src="assets/images/dev.png" alt="logo" height ="100em" />  Code Therapy</h1>
     <events-list></events-list>
 
